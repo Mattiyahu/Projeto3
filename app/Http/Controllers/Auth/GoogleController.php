09890811a21d1aabf1e449d2fa8af956d2748394
unknown <thepancakeface823@gmail.com>
2025-02-27 08:45:04 -0300

    Primeiro commit

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $user = Socialite::driver('google')->user();
+
+        $findUser = User::where('email', $user->email)->first();
+
+        if ($findUser) {
+            Auth::login($findUser);
+
+            return redirect()->intended('dashboard');
+        } else {
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_id' => $user->id,
+                'password' => Hash::make(uniqid())
+            ]);
+
+            Auth::login($newUser);
+
+            return redirect()->intended('dashboard');
+        }
+    }
+}
